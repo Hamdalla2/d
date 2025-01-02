cf0fc2b50cefc39ec9afbfebfdbddd90b7d8fc38
Hamdallah Fatayer <hamdalla9@gmail.com>
2025-01-02 11:57:17 +0200

    .

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,9 +1,24 @@
 
 import { Link } from "react-router-dom";
 function Header() {
-    return <header style={{ position: "sticky", display: "flex", alignItems: "center" }}>
+    return <header>
         <Link to="/">
             <img alt="logo" width={50} height={50} src="https://lh4.googleusercontent.com/proxy/X1ggCm_dY7EVJsjmH7tL-L8F3QOg5atSBi3fYZa_XynlWW5B6J3nbEi-iDZz834ff6vsEIph0cyF1yrf0crgPqzg_e63jREZBKRVS-f6RH0U5Tt3-A_sC8DbhCiRB768_ZLBtA"></img>
+        </Link>
+        <Link to="/">
+            <button>Patients</button>
+        </Link>
+        <Link to="/">
+            <button>Add</button>
+        </Link>
+        <Link to="/">
+            <button>Delete</button>
+        </Link>
+        <Link to="/">
+            <button>Calendar</button>
+        </Link>
+        <Link to="/">
+            <button>Today</button>
         </Link>
         <Link to="/">
             <button>Sign In</button>
